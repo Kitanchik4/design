a7a6608bf0a72bf579ca0bf8bd412abe28445689
Алексей <alexmatyrno@ya.ru>
2024-05-13 21:28:10 +0300

    Fail

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -31,6 +31,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            label5 = new Label();
+            label4 = new Label();
+            button5 = new Button();
+            textBox2 = new TextBox();
+            comboBox2 = new ComboBox();
             comboBox1 = new ComboBox();
             textBox1 = new TextBox();
             monthCalendar1 = new MonthCalendar();
@@ -42,24 +47,23 @@
             listView1 = new ListView();
             columnHeader1 = new ColumnHeader();
             columnHeader2 = new ColumnHeader();
-            tabPage2 = new TabPage();
-            button4 = new Button();
-            button3 = new Button();
-            comboBox2 = new ComboBox();
-            label5 = new Label();
-            label4 = new Label();
-            textBox2 = new TextBox();
-            listView2 = new ListView();
-            columnHeader3 = new ColumnHeader();
-            pictureBox1 = new PictureBox();
-            pictureBox2 = new PictureBox();
             columnHeader4 = new ColumnHeader();
+            columnHeader10 = new ColumnHeader();
+            columnHeader9 = new ColumnHeader();
+            pictureBox1 = new PictureBox();
+            tabPage2 = new TabPage();
+            button3 = new Button();
+            listView2 = new ListView();
             columnHeader5 = new ColumnHeader();
+            columnHeader8 = new ColumnHeader();
+            columnHeader7 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
             columnHeader6 = new ColumnHeader();
+            pictureBox2 = new PictureBox();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
-            tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
@@ -70,12 +74,17 @@
             tabControl1.Location = new Point(12, 12);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(685, 426);
+            tabControl1.Size = new Size(742, 426);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
             tabPage1.BackColor = SystemColors.InactiveCaption;
+            tabPage1.Controls.Add(label5);
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(button5);
+            tabPage1.Controls.Add(textBox2);
+            tabPage1.Controls.Add(comboBox2);
             tabPage1.Controls.Add(comboBox1);
             tabPage1.Controls.Add(textBox1);
             tabPage1.Controls.Add(monthCalendar1);
@@ -89,9 +98,59 @@
             tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(677, 398);
+            tabPage1.Size = new Size(734, 398);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Задачи";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.White;
+            label5.Location = new Point(301, 96);
+            label5.Name = "label5";
+            label5.Size = new Size(91, 15);
+            label5.TabIndex = 11;
+            label5.Text = "Оценка задачи:";
+            label5.Click += label5_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.White;
+            label4.Location = new Point(302, 53);
+            label4.Name = "label4";
+            label4.Size = new Size(90, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Номер Задачи:";
+            label4.Click += label4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(212, 319);
+            button5.Name = "button5";
+            button5.Size = new Size(93, 64);
+            button5.TabIndex = 1;
+            button5.Text = "Добавить в выполненные задачи";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(395, 50);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Отлично", "Хорошо", "Можно было лучше", "Плохо" });
+            comboBox2.Location = new Point(395, 93);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(121, 23);
+            comboBox2.TabIndex = 5;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
             // 
             // comboBox1
             // 
@@ -101,6 +160,7 @@
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(121, 23);
             comboBox1.TabIndex = 8;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // textBox1
             // 
@@ -108,12 +168,14 @@
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(100, 23);
             textBox1.TabIndex = 7;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // monthCalendar1
             // 
-            monthCalendar1.Location = new Point(382, 85);
+            monthCalendar1.Location = new Point(558, 56);
             monthCalendar1.Name = "monthCalendar1";
             monthCalendar1.TabIndex = 6;
+            monthCalendar1.DateChanged += monthCalendar1_DateChanged;
             // 
             // label3
             // 
@@ -124,6 +186,7 @@
             label3.Size = new Size(102, 15);
             label3.TabIndex = 5;
             label3.Text = "Название задачи:";
+            label3.Click += label3_Click;
             // 
             // label2
             // 
@@ -134,136 +197,140 @@
             label2.Size = new Size(112, 15);
             label2.TabIndex = 4;
             label2.Text = "Готовность задачи:";
+            label2.Click += label2_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.White;
-            label1.Location = new Point(391, 61);
+            label1.Location = new Point(569, 32);
             label1.Name = "label1";
             label1.Size = new Size(143, 15);
             label1.TabIndex = 3;
             label1.Text = "Срок выполнения задач:";
+            label1.Click += label1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(35, 275);
+            button2.Location = new Point(17, 319);
             button2.Name = "button2";
             button2.Size = new Size(90, 64);
             button2.TabIndex = 2;
             button2.Text = "Добавить задачу";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(181, 275);
+            button1.Location = new Point(113, 319);
             button1.Name = "button1";
             button1.Size = new Size(93, 64);
             button1.TabIndex = 1;
             button1.Text = "Удалить задачу";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // listView1
             // 
-            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader4 });
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader4, columnHeader10, columnHeader9 });
             listView1.GridLines = true;
-            listView1.Location = new Point(35, 134);
+            listView1.Location = new Point(17, 158);
             listView1.Name = "listView1";
-            listView1.Size = new Size(278, 135);
+            listView1.Size = new Size(510, 155);
             listView1.TabIndex = 0;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged_1;
             // 
             // columnHeader1
             // 
-            columnHeader1.Text = "Задачи";
+            columnHeader1.Text = "Название Задачи";
+            columnHeader1.Width = 120;
             // 
             // columnHeader2
             // 
             columnHeader2.Text = "Готовность";
             columnHeader2.Width = 100;
             // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Срок выполнения";
+            columnHeader4.Width = 120;
+            // 
+            // columnHeader10
+            // 
+            columnHeader10.DisplayIndex = 4;
+            columnHeader10.Text = "Номер задачи";
+            columnHeader10.Width = 100;
+            // 
+            // columnHeader9
+            // 
+            columnHeader9.DisplayIndex = 3;
+            columnHeader9.Text = "Оценка";
+            columnHeader9.Width = 80;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.ErrorImage = (Image)resources.GetObject("pictureBox1.ErrorImage");
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.InitialImage = (Image)resources.GetObject("pictureBox1.InitialImage");
+            pictureBox1.Location = new Point(-105, -64);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(909, 466);
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
             // tabPage2
             // 
             tabPage2.BackColor = Color.WhiteSmoke;
-            tabPage2.Controls.Add(button4);
             tabPage2.Controls.Add(button3);
-            tabPage2.Controls.Add(comboBox2);
-            tabPage2.Controls.Add(label5);
-            tabPage2.Controls.Add(label4);
-            tabPage2.Controls.Add(textBox2);
             tabPage2.Controls.Add(listView2);
             tabPage2.Controls.Add(pictureBox2);
             tabPage2.Location = new Point(4, 24);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(677, 398);
+            tabPage2.Size = new Size(734, 398);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Выполненные задачи";
             // 
-            // button4
-            // 
-            button4.Location = new Point(171, 178);
-            button4.Name = "button4";
-            button4.Size = new Size(127, 62);
-            button4.TabIndex = 7;
-            button4.Text = "Поставить оценку";
-            button4.UseVisualStyleBackColor = true;
-            // 
             // button3
             // 
-            button3.Location = new Point(41, 178);
+            button3.Location = new Point(129, 314);
             button3.Name = "button3";
-            button3.Size = new Size(124, 62);
+            button3.Size = new Size(206, 58);
             button3.TabIndex = 6;
             button3.Text = "Удалить выполненную задачу";
             button3.UseVisualStyleBackColor = true;
-            // 
-            // comboBox2
-            // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Items.AddRange(new object[] { "Отлично", "Хорошо", "Можно было лучше", "Плохо" });
-            comboBox2.Location = new Point(132, 100);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(121, 23);
-            comboBox2.TabIndex = 5;
-            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(38, 103);
-            label5.Name = "label5";
-            label5.Size = new Size(88, 15);
-            label5.TabIndex = 4;
-            label5.Text = "Оценка задачи";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(41, 54);
-            label4.Name = "label4";
-            label4.Size = new Size(85, 15);
-            label4.TabIndex = 3;
-            label4.Text = "Номер задачи";
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(132, 51);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
-            textBox2.TabIndex = 1;
+            button3.Click += button3_Click;
             // 
             // listView2
             // 
-            listView2.Columns.AddRange(new ColumnHeader[] { columnHeader6, columnHeader5, columnHeader3 });
+            listView2.Columns.AddRange(new ColumnHeader[] { columnHeader5, columnHeader8, columnHeader7, columnHeader3, columnHeader6 });
             listView2.GridLines = true;
-            listView2.Location = new Point(351, 51);
+            listView2.Location = new Point(36, 29);
             listView2.Name = "listView2";
-            listView2.Size = new Size(293, 325);
+            listView2.Size = new Size(420, 279);
             listView2.TabIndex = 0;
             listView2.UseCompatibleStateImageBehavior = false;
             listView2.View = View.Details;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.DisplayIndex = 1;
+            columnHeader5.Text = "Название задачи";
+            columnHeader5.Width = 120;
+            // 
+            // columnHeader8
+            // 
+            columnHeader8.DisplayIndex = 4;
+            columnHeader8.Text = "Статус";
+            // 
+            // columnHeader7
+            // 
+            columnHeader7.DisplayIndex = 3;
+            columnHeader7.Text = "Дата";
+            columnHeader7.Width = 80;
             // 
             // columnHeader3
             // 
@@ -271,39 +338,20 @@
             columnHeader3.Text = "№Задачи";
             columnHeader3.Width = 80;
             // 
-            // pictureBox1
+            // columnHeader6
             // 
-            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(-4, 0);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(681, 398);
-            pictureBox1.TabIndex = 9;
-            pictureBox1.TabStop = false;
+            columnHeader6.DisplayIndex = 2;
+            columnHeader6.Text = "Оценка";
+            columnHeader6.Width = 80;
             // 
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
             pictureBox2.Location = new Point(0, 0);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(677, 398);
+            pictureBox2.Size = new Size(734, 398);
             pictureBox2.TabIndex = 8;
             pictureBox2.TabStop = false;
-            // 
-            // columnHeader4
-            // 
-            columnHeader4.Text = "Срок выполнения";
-            columnHeader4.Width = 120;
-            // 
-            // columnHeader5
-            // 
-            columnHeader5.Text = "Название задачи";
-            columnHeader5.Width = 140;
-            // 
-            // columnHeader6
-            // 
-            columnHeader6.DisplayIndex = 2;
-            columnHeader6.Text = "Оценка";
-            columnHeader6.Width = 70;
             // 
             // Form1
             // 
@@ -316,9 +364,8 @@
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
             tabPage1.PerformLayout();
-            tabPage2.ResumeLayout(false);
-            tabPage2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
         }
@@ -340,17 +387,21 @@
         private ComboBox comboBox1;
         private TextBox textBox1;
         private ListView listView2;
-        private Label label5;
-        private Label label4;
         private TextBox textBox2;
         private ColumnHeader columnHeader3;
         private ComboBox comboBox2;
-        private Button button4;
         private Button button3;
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
         private ColumnHeader columnHeader4;
         private ColumnHeader columnHeader6;
         private ColumnHeader columnHeader5;
+        private Button button5;
+        private ColumnHeader columnHeader7;
+        private ColumnHeader columnHeader8;
+        private ColumnHeader columnHeader9;
+        private ColumnHeader columnHeader10;
+        private Label label5;
+        private Label label4;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -15,6 +17,110 @@ namespace WinFormsApp1
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string taskName = textBox1.Text;
+            string taskCategory = comboBox1.Text;
+            string taskDate = monthCalendar1.SelectionStart.ToShortDateString();
+            string taskNumber = textBox2.Text;
+            string taskGrade = comboBox2.Text;
+
+            ListViewItem newTask = new ListViewItem(taskName);
+            newTask.SubItems.Add(taskCategory);
+            newTask.SubItems.Add(taskDate);
+            newTask.SubItems.Add(taskNumber);
+            newTask.SubItems.Add(taskGrade);
+            listView1.Items.Add(newTask);
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                listView1.Items.Remove(listView1.SelectedItems[0]);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                foreach (ListViewItem item in listView1.SelectedItems)
+                {
+                    listView2.Items.Add((ListViewItem)item.Clone());
+                    listView1.Items.Remove(item);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listView2.SelectedItems.Count > 0)
+            {
+                listView2.Items.Remove(listView2.SelectedItems[0]);
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
